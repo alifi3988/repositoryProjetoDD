43db70c4dfbea702ba6352e158fb0687e8ecd2a0
alifi3988 <alifi3988@gmail.com>
2024-04-02 07:59:48 -0300

    Criação do projeto inicial

---
+++ projectDD/src/main/java/br/com/estudos/decode/project/ProjectDdApplication.java
@@ -0,0 +1,13 @@
+package br.com.estudos.decode.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectDdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectDdApplication.class, args);
+	}
+
+}
---
+++ projectDD/src/test/java/br/com/estudos/decode/project/ProjectDdApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.estudos.decode.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectDdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
